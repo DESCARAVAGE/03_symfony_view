6797bb8277aa425506b0fd2359c93937c4c43037
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-24 20:42:24 +0200

    jointure des tables

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    const SERIES = [
+        ['title' => 'Naruto', 'synopsis' => 'Les aventures des ninjas de konoha', 'category' => 'Aventure'],
+        ['title' => 'MHA', 'synopsis' => 'Les aventures des heros', 'category' => 'Action'],
+        ['title' => 'DBS', 'synopsis' => 'Des combats de saiyans', 'category' => 'Fantastique'],
+        ['title' => 'Parasite', 'synopsis' => 'Des parasites qui vous rentre dedans', 'category' => 'Fantastique'],
+        ['title' => 'overlord', 'synopsis' => 'Les aventures de Ainz dans un RPG', 'category' => 'Aventure'],
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::SERIES as $seriesName) {
+            $program = new Program();
+            $program->setTitle($seriesName ['title']);
+            $program->setSynopsis($seriesName['synopsis']);
+            $program->setCategory($this->getReference('category_' . $seriesName['category']));
+            $manager->persist($program);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+}
