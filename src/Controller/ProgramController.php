e1cbc2ced584ed02733dc8486eb8f91ebfb9910e
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-25 09:32:18 +0200

    ajout d'un bouton de redirection, des fiches des series

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,18 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/', name: 'app_index')]
+    public function index(ProgramRepository $programRepository): Response
     {
+        
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'programs' => $programs,
          ]);
     }
+    
     #[Route('/{id}', methods:['GET'], requirements:['id'=>'\d+'], name: 'show')]
-    public function show(int $id): Response
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
