d10ba3a0b0812eb937f5ccfa4096e9c6e2d8de77
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-26 12:02:05 +0200

    création des entités season et episode et modif de program

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,17 +22,24 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'show')]
-    public function show(int $id, ProgramRepository $programRepository): Response
+    public function show(int $id, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
     {
         $program = $programRepository->findOneBy(['id' => $id]);
         if (!$program) {
             throw $this->createNotFoundException(
                 'No program with id : ' . $id . ' found in program\'s table.'
             );
-        } else {
-            return $this->render('program/show.html.twig', [
-                'program' => $program,
-            ]);
         }
+        $seasons = $seasonRepository->findByProgram(['program' => $program]);
+        if (!$seasons) {
+            throw $this->createNotFoundException(
+                'No season with id : ' . $id . ' found in program\'s table.'
+            );
+        }
+        
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+            'season' => $seasons,
+        ]);
     }
 }
