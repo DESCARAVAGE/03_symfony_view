e003bcc49f2c830f6d68a5fe0106f4fb46c3b1ab
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-29 19:39:00 +0200

    ajout d'une categorie et/ou d'une serie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,11 +2,16 @@
 // src/Controller/CategoryController.php
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
@@ -18,6 +23,31 @@ class CategoryController extends AbstractController
         return $this->render('category/index.html.twig', [
             'categories' => $categories,
         ]);
+    }
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+        $formCategory = $this->createForm(CategoryType::class, $category);
+        $formCategory->handleRequest($request);
+        if ($formCategory->isSubmitted()) {
+            $categoryRepository->add($category, true);  
+            // Deal with the submitted data
+            // For example : persiste & flush the entity
+            // And redirect to a route that display the result
+            return $this->redirectToRoute('category_index');
+        }
+
+        
+        return $this->renderForm('category/new.html.twig', [
+            'formCategory' => $formCategory,
+        ]);
+
+        // Alternative
+        // return $this->render('category/new.html.twig', [
+        //   'form' => $form->createView(),
+        // ]);
     }
 
     #[Route('/{categoryName}', methods: ['GET'], name: 'show')]
