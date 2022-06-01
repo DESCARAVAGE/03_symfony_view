fa6c7e4bd65ee7d7c95283b548cf7308b9747860
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-06-01 21:52:01 +0200

    ajout de la liste des actors pour créer une nouvelle série

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -13,6 +15,10 @@ class ProgramType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('actors', EntityType::class, ['class' => Actor::class,
+            'choice_label' => 'name',
+            'multiple' => true,
+            'expanded' => true,])
             ->add('title', TextType::class, ['label' => 'Titre'])
             ->add('synopsis', TextType::class)
             ->add('poster', TextType::class, ['required'=> false, 'label' => 'Affiche'])
